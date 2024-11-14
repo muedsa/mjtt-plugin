c6aab03c546bf33ee848af00d41d1d1b9b5fe53b
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-11-14 17:49:55 +0800

    test: 对新代码的适配

--- app/src/test/java/com/muedsa/tvbox/mjtt/service/MJTTServiceTest.kt
+++ app/src/test/java/com/muedsa/tvbox/mjtt/service/MJTTServiceTest.kt
@@ -1,12 +1,21 @@
 package com.muedsa.tvbox.mjtt.service
 
+import com.muedsa.tvbox.tool.IPv6Checker
+import com.muedsa.tvbox.tool.createOkHttpClient
 import org.junit.Test
 
 class MJTTServiceTest {
 
+    private val okHttpClient = createOkHttpClient(
+        debug = true,
+        onlyIpv4 = IPv6Checker.checkIPv6Support() != IPv6Checker.IPv6Status.SUPPORTED
+    )
+
     @Test
     fun getSiteUrlTest() {
-        val siteUrl = MJTTService.getSiteUrl()
+        val siteUrl = MJTTService(
+            okHttpClient = okHttpClient
+        ).getSiteUrl()
         check(siteUrl.isNotBlank())
         println(siteUrl)
     }
