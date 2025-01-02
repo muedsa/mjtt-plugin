35febfa6b277d888c2cce1b28831907413e3ca60
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-01-02 11:05:30 +0800

    fix: serverName is nullable

--- app/src/main/java/com/muedsa/tvbox/mjtt/model/PlayUrlData.kt
+++ app/src/main/java/com/muedsa/tvbox/mjtt/model/PlayUrlData.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PlayUrlData(
-    @SerialName("servername") val serverName: String,
+    @SerialName("servername") val serverName: String? = null,
     @SerialName("playname") val playName: String,
     @SerialName("playurls") val playUrls: List<List<String>>, // [[name,source,pageUrl]]
 )
