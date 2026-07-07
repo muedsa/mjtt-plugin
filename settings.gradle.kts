44263dfcc3af89705797afb154c00be1a122a43d
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-07-07 17:51:33 +0800

    chore: 适配API13

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "mjtt-plugin"
 include(":app")
-include(":api")
-project(":api").projectDir = rootDir.resolve("./TvBoxPlugin/api/")
+includeBuild("TvBoxPlugin")
